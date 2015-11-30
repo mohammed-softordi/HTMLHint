5e1ed300a48b99bf84392e6de9448a3cc27edb63
Mohammed Ayowa <mohammed.ayowa@scania.com>
2015-11-30 17:41:39 +0100

    added icon-class-deprecated rule spec

--- test/rules/icon-class-deprecated.spec.js
+++ test/rules/icon-class-deprecated.spec.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2015, Yanis Wang <yanis.wang@gmail.com>
+ * Created by maya5j on 2015-11-30 (Mohammed Ayowa) <mohammed.ayowa@scania.com>
  * MIT Licensed
  */
 
@@ -17,13 +17,12 @@ describe('Rules: '+ruldId, function(){
     it('class icon should result in an error', function(){
         var code = '<body><div class="icon-danger"></div></body>';
         var messages = HTMLHint.verify(code, ruleOptions);
-        console.log(messages);
+		
         expect(messages.length).to.be(1);
         expect(messages[0].rule.id).to.be(ruldId);
         expect(messages[0].line).to.be(1);
         expect(messages[0].col).to.be(11);
         expect(messages[0].type).to.be('warning');
-
     });
 
 });
